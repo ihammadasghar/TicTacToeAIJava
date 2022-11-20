dfb3a65c38a17d4ccec6db3c67b1d5f179bfeeae
Hammad Asghar <ihammadasghar@gmail.com>
2022-11-20 15:52:06 +0000

    [task-15] Added SetupFrame

---
+++ src/Views/PlayersSetupPanel.java
@@ -0,0 +1,17 @@
+package Views;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class PlayersSetupPanel extends JPanel implements ActionListener {
+    public PlayersSetupPanel() {
+        super();
+        
+    }
+
+    public void actionPerformed(ActionEvent e) {
+    }
+
+}
