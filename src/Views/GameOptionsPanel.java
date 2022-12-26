8c6689816da988debe57668b6bcf1ff08d890e9f
Du4rteRodrigues <duarte.m.rodrigues@sapo.pt>
2022-12-26 18:51:44 +0000

    SetUpFrame fix

--- src/Views/GameOptionsPanel.java
+++ src/Views/GameOptionsPanel.java
@@ -11,31 +11,32 @@ public class GameOptionsPanel extends JPanel implements ActionListener {
 
     public GameOptionsPanel() {
         super();
+        this.setLayout(new GridLayout(0, 1, 10, 0));
         JPanel optionsPanel = new JPanel();
         JPanel title = new JPanel();
         JPanel empty = new JPanel();
-        title.setBorder(GUI.blackline);
+
+        JLabel label = new JLabel("Game Options", SwingConstants.CENTER);
+
         optionsPanel.setBorder(GUI.blackline);
         optionsPanel.setLayout(new GridLayout(3,1));
 
         title.setBorder(new EmptyBorder(100, 10, 50, 10));
         title.setLayout(new GridBagLayout());
-        GridBagConstraints gbc = new GridBagConstraints();
 
+        GridBagConstraints gbc = new GridBagConstraints();
         gbc.gridwidth = GridBagConstraints.REMAINDER;
         gbc.anchor = GridBagConstraints.CENTER;
         gbc.fill = GridBagConstraints.HORIZONTAL;
 
-        this.setLayout(new GridLayout(0, 1, 10, 0));
-        JLabel label = new JLabel("Game Options", SwingConstants.CENTER);
         label.setPreferredSize(new Dimension(300,200));
         label.setForeground(Color.BLUE);
         label.setFont(GUI.setupPanelTitleFont);
         label.setPreferredSize(new Dimension(500, 50));
+
         title.add(label);
 
         ButtonGroup options = new ButtonGroup();
-
         for (int i = 0; i < optionRadioBtns.length; i++) {
             optionRadioBtns[i] = new JRadioButton(GUI.gameTypeOptions[i]);
             Font radioButtonFont= new Font(optionRadioBtns[i].getFont().getName(),optionRadioBtns[i].getFont().getStyle(),18);
