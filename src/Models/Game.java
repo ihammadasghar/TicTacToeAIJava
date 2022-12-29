bc34fb58432ceeb8b71420475d2d4b95c71a8479
Hammad Asghar <ihammadasghar@gmail.com>
2022-12-29 14:29:29 +0000

    [task-42] updated models

--- src/Models/Game.java
+++ src/Models/Game.java
@@ -7,6 +7,8 @@ public class Game {
     public Player[] players;
     public int currentPlayerNum;
     public int[][] grid;
+    public int movesMade = 0;
+    public int[] lastMoveMade = new int[2];
 
     public Game(String type, int win, int gridSize, Player[] players) {
         this.type = type;
@@ -26,68 +28,4 @@ public class Game {
         return false;
     }
 
-    public int calculatePlayerScore(int playerNum) {
-        return getRowsScore(playerNum) + getColsScore(playerNum) + getDiag1Score(playerNum) + getDiag2Score(playerNum);
-    }
-
-    private int getColsScore(int playerNum) {
-        int score = 0;
-        for (int row = 0; row < this.gridSize; row++) {
-            int count = 0;
-            for (int col = 0; col < this.gridSize; col++) {
-                count = playerNum != this.grid[row][col] ? 0 : count + 1;
-
-                if (count == this.win) {
-                    score++;
-                    count--;
-                }
-            }
-        }
-        return score;
-    }
-
-    private int getRowsScore(int playerNum) {
-        int score = 0;
-        for (int col = 0; col < this.gridSize; col++) {
-            int count = 0;
-            for (int row = 0; row < this.gridSize; row++) {
-                count = playerNum != this.grid[row][col] ? 0 : count + 1;
-
-                if (count == this.win) {
-                    score++;
-                    count--;
-                }
-            }
-        }
-        return score;
-    }
-
-    private int getDiag1Score(int playerNum) {
-        int score = 0;
-        int count = 0;
-        for (int i = 0; i < this.gridSize; i++) {
-            count = playerNum != this.grid[i][i] ? 0 : count + 1;
-
-            if (count == this.win) {
-                score++;
-                count--;
-            }
-        }
-        return score;
-    }
-
-    private int getDiag2Score(int playerNum) {
-        int score = 0;
-        int count = 0;
-        for (int i = 0; i < this.gridSize; i++) {
-            int col = this.gridSize - 1 - i;
-            count = playerNum != this.grid[i][col] ? 0 : count + 1;
-
-            if (count == this.win) {
-                score++;
-                count--;
-            }
-        }
-        return score;
-    }
 }
