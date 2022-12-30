08c3080146a161d4d10bf4396e839470e955dc32
Duarte Rodrigues <93005604+Du4rteRodrigues@users.noreply.github.com>
2022-12-30 18:59:21 +0000

    Update RestartPanel.java

--- src/Views/RestartPanel.java
+++ src/Views/RestartPanel.java
@@ -9,16 +9,6 @@ import java.awt.event.ActionListener;
 public class RestartPanel extends JPanel implements ActionListener {
 
     public RestartPanel() {
-        //this.setBorder(new EmptyBorder(20, 30, 50, 10));
-        //this.setLayout(new GridBagLayout());
-
-        /*
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.gridwidth = GridBagConstraints.REMAINDER;
-        gbc.anchor = GridBagConstraints.CENTER;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-         */
-
         JButton restartBtn = new JButton("Restart");
         restartBtn.setFont(GUI.startFont);
         restartBtn.addActionListener(this);
